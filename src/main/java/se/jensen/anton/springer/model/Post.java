918f00572ec4faf743ad848d97d4306d7819cc49
AntonHenriksson <henriksson.anton@gmail.com>
2025-12-03 14:48:50 +0100

    wed

---
+++ src/main/java/se/jensen/anton/springer/model/Post.java
@@ -0,0 +1,10 @@
+package se.jensen.anton.springer.model;
+
+import java.time.LocalDateTime;
+
+public class PostDTO {
+    private Long id = 0L;
+    private String title;
+    private LocalDateTime created;
+    
+}
