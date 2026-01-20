77a9d3a5ddc35b9d34a141b626135fe31dbfd9cd
AntonHenriksson <henriksson.anton@gmail.com>
2026-01-20 22:33:10 +0100

    tredje commit med en FriendRequest entititet
    fyllt controller inför service

--- src/main/java/se/jensen/anton/springer/controller/FriendshipController.java
+++ src/main/java/se/jensen/anton/springer/controller/FriendshipController.java
@@ -1,11 +1,31 @@
 package se.jensen.anton.springer.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.jensen.anton.springer.dto.FriendshipRespondDTO;
 
 @RestController
 @RequestMapping("/friendship")
 public class FriendshipController {
 
+    @GetMapping("/{id}")
+    public ResponseEntity<FriendshipRespondDTO> get(@RequestParam Long id) {
+        return ResponseEntity.ok(null); //service här med lista på friendships
+    }
+
+    @GetMapping("/users/{id}/friends")
+    public ResponseEntity<FriendshipRespondDTO> getFriends(@PathVariable Long id) {
+        return ResponseEntity.ok(null); // service här med /user/id/friends
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FriendshipRespondDTO> accept(@PathVariable Long id) {
+        return ResponseEntity.ok(null); //service här med status update
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FriendshipRespondDTO> reject(@PathVariable Long id) {
+        return ResponseEntity.ok(null); //service här med status reject
+    }
 
 }
